e8d9b399f9a2fbaaa4c19134682634de549f1f69
Ridwan <lawalridwan860@gmail.com>
2023-08-05 18:57:37 +0100

    built the input component

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      mono: ["Space Mono", "monospace"],
+    },
+    extend: {
+      colors: {
+        veryDarkCyan: "hsl(183, 100%, 15%)",
+        darkGrayishCyan: "hsl(186, 14%, 43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLIghtGrayishCyan: "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
